ae8b1645761372289a5d346eabd1db595d5ddf3c
Riad Mashrub Shourov <shourovrm@gmail.com>
2026-01-02 23:41:55 +0000

    edited home.nix for tty mangorc

--- home/rms/home.nix
+++ home/rms/home.nix
@@ -7,6 +7,19 @@
 
 	programs.git.enable = true;
 
+	## for TTY session of mangorc
+	home.sessionVariables = {
+		XDG_SESSION_TYPE = "wayland";
+		XDG_CURRENT_DESKTOP = "mangorc";
+		XDG_SESSION_DESKTOP = "mangorc";
+
+		MOZ_ENABLE_WAYLANG = "1";
+		QT_QPA_PLATFORM = "wayland";
+		SDL_VIDEODRIVER = "wayland";
+
+		_JAVA_AWT_WM_NONREPARENTING = "1";
+	};
+
 	programs.neovim.enable = true;
 	programs.neovim.defaultEditor = true;
 
